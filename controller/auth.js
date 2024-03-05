b60d79dfa52615affad45819cb05fe04d879bf03
bennetbvarghese <agent98bp@gmail.com>
2024-03-05 21:11:37 +0530

    commit

--- controller/auth.js
+++ controller/auth.js
@@ -2,19 +2,19 @@ import User from '../models/UserModel.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { UserName, FirstName, LastName,Password,Email,PhoneNumber,Address,BloodType,MedicalHistory,Availability,Consent} = req.body;
   try {
-    // if(username){
-    //   console.log('Username:', username);
-    //   return res.status(400).json('Username already exists!');
-    // }
-    const hashedPassword = await bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
+    const existingUser = await User.findOne({ Email });
+    if(existingUser){
+       console.log('Username:', existingUser);
+       return res.status(400).json('Email already exists!');
+     }
+    const hashedPassword = await bcryptjs.hashSync(Password, 10);
+    const newUser = new User({ UserName, FirstName, LastName,Password:hashedPassword ,Email,PhoneNumber,Address,BloodType,MedicalHistory,Availability,Consent});
     await newUser.save();
     res.status(201).json('User created successfully!');
-    console.log( newUser
+    console.log(newUser
     );
   } catch (error) {
     res.status(500).json('Internal Server Error');
@@ -22,11 +22,11 @@ export const signup = async (req, res, next) => {
   }
 };
 export const signin = async (req, res, next) => {
-    const { email, password } = req.body;
+    const { Email, Password } = req.body;
     try {
-      const validUser = await User.findOne({ email });
+      const validUser = await User.findOne({ Email });
       if (!validUser) return next(errorHandler(404, 'User not found!'));
-      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      const validPassword = bcryptjs.compareSync(Password, validUser.Password);
       if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
       const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = validUser._doc;
--- models/UserModel.js
+++ models/UserModel.js
@@ -3,9 +3,17 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  UserName: { type: String, required: true, unique: true },
+  FirstName: { type: String, required: true },
+  LastName: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true, unique: true },
+  PhoneNumber: { type: String, required: true },
+  Address: { type: String, required: true },
+  BloodType: { type: String, required: true },
+  MedicalHistory: { type: String, default: '' },
+  Availability: { type: String, default: '' },
+  Consent: { type: Boolean, required: true }
 });
 
 const UserModel = mongoose.model('User', userSchema);
